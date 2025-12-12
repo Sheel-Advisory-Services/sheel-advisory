b8c777b9d51a42f72653fa9fa7241e3e57a669b8
Lucia <luciasheel@gmail.com>
2025-12-12 09:36:44 -0800

    Add back-to-top button with smooth scroll and fade-in animation

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Props from '@/components/Props';
 // import Testimonials from '@/components/Testimonials';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
+import BackToTop from '@/components/BackToTop';
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
       {/* <Testimonials /> */}
       <Contact />
       <Footer />
+      <BackToTop />
     </main>
   );
 }
